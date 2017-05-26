5746a5213d1b17d39d28b8468477802d61863386
Drew Larson <drewelizabeth@Drews-MacBook-Air.local>
2017-05-25 18:00:12 -0600

    line and bar directives working; pie and doughnut not rendering

--- client_source/app/directives/doughnutDirective.js
+++ client_source/app/directives/doughnutDirective.js
@@ -1,10 +1,45 @@
 // Start: This is the doughnut chart directive =================================
-AA.directive('doughnutDirective', function () {
-
+AA.directive('doughnutDirective', function() {
   return {
     restrict: 'E',
-    templateUrl: './views/doughnut.html',
-    controller: 'mainCtrl'
-  };
+    templateUrl: "./../views/doughnut.html",
+    // controller: 'dirCtrl',
+    scope: {
+      chartData: '=',
+      type: "="
+    },
+    link: function(scope, elem, attrs, ctrl) {
+      console.log('this is my element\'s second child:', elem[0].children[1].children[0]);
+
+      let ctxDir = elem[0].children[1].children[0];
+
+      let myChartDir = getChartGivenData(ctxDir, scope.chartData);
+
+      function getChartGivenData(chartElement, dataForChart) {
+        return new Chart(
+          chartElement,
+          {
+            type: scope.type,
+            data: dataForChart,
+            options: {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }]
+              }
+            }
+          }
+        );
+      }
+
+      setInterval(() => {
+        getChartGivenData(ctxDir, scope.chartData);
+        // myChartDir.update();
+      }, 5000);
+    }
+  }
 });
+
 // End: This is the doughnut chart directive ===================================
--- public_dist/bundle.js
+++ public_dist/bundle.js
@@ -12,10 +12,53 @@ var AA = angular.module("data-dash", []);
 /* ============================================================================= */
 /* ======================== Start: Main Controller ============================= */
 /* ============================================================================= */
-AA.controller("mainCtrl", ["$scope", function ($scope) {
+AA.controller("mainCtrl", ["$scope", "$timeout", function ($scope, $timeout) {
+  $scope.baseball = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Test1", "Test2"],
+    datasets: [{
+      label: '# of Votes',
+      data: [1290, 2283, 3092, 1532, 2111, 1003, 1487, 2645],
+      backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 159, 64, 0.2)'],
+      borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(255, 159, 64, 1)', 'rgba(255, 159, 64, 1)'],
+      borderWidth: 1
+    }]
+  };
 
-    //$scope.test = "bOYAh\! the app is working";
+  $scope.lineData = {
+    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    datasets: [{
+      label: 'apples',
+      data: [12, 19, 3, 17, 6, 3, 7],
+      backgroundColor: "rgba(153,255,51,0.4)"
+    }, {
+      label: 'oranges',
+      data: [2, 29, 5, 5, 2, 3, 10],
+      backgroundColor: "rgba(255,153,0,0.4)"
+    }]
+  };
 
+  $scope.roundDate = {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    datasets: [{
+      backgroundColor: ["#2ecc71", "#3498db", "#95a5a6", "#9b59b6", "#f1c40f", "#e74c3c", "#34495e"],
+      data: [12, 19, 3, 17, 28, 24, 7]
+    }]
+  };
+
+  $scope.chart1Type = 'line';
+  $scope.chart2Type = 'bar';
+  $scope.chart3Type = 'pie';
+  $scope.chart4Type = 'doughnut';
+
+  $timeout(function () {
+    $scope.$applyAsync(function () {
+      $scope.chart1Type = $scope.chart1Type === 'bar' ? 'line' : 'bar';
+      $scope.chart2Type = $scope.chart1Type === 'bar' ? 'line' : 'bar';
+      // $scope.chart3Type = $scope.chart3Type === 'pie' ? 'doughnut' : 'pie';
+      // $scope.chart4Type = $scope.chart3Type === 'pie' ? 'doughnut' : 'pie';
+      $scope.baseball.labels = ["Rojo", "Azul", "Yellow", "Green", "Purple", "Orange", "Test1", "Test2"];
+    });
+  }, 3000);
 }]);
 /* ============================================================================= */
 /* ======================== End: Main Controller =============================== */
@@ -24,13 +67,45 @@ AA.controller("mainCtrl", ["$scope", function ($scope) {
 
 // Start: This is the doughnut chart directive =================================
 AA.directive('doughnutDirective', function () {
-
   return {
     restrict: 'E',
-    templateUrl: './views/doughnut.html',
-    controller: 'mainCtrl'
+    templateUrl: "./../views/doughnut.html",
+    // controller: 'dirCtrl',
+    scope: {
+      chartData: '=',
+      type: "="
+    },
+    link: function link(scope, elem, attrs, ctrl) {
+      console.log('this is my element\'s second child:', elem[0].children[1].children[0]);
+
+      var ctxDir = elem[0].children[1].children[0];
+
+      var myChartDir = getChartGivenData(ctxDir, scope.chartData);
+
+      function getChartGivenData(chartElement, dataForChart) {
+        return new Chart(chartElement, {
+          type: scope.type,
+          data: dataForChart,
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
+          }
+        });
+      }
+
+      setInterval(function () {
+        getChartGivenData(ctxDir, scope.chartData);
+        // myChartDir.update();
+      }, 5000);
+    }
   };
 });
+
 // End: This is the doughnut chart directive ===================================
 'use strict';
 
