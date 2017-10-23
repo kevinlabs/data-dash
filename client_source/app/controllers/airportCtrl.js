273b504966a4c1f49030491539fc458b31269305
Kevin Labs <kevinlabx@gmail.com>
2017-10-23 15:14:43 -0500

    -Fixed the error issue from airportCtrl.

--- client_source/app/controllers/airportCtrl.js
+++ client_source/app/controllers/airportCtrl.js
@@ -4,13 +4,5 @@ AA.controller("airportCtrl", function($scope, zipConversionService){
     console.log(data);
       $scope.airport = data.airport; // AIRPORT
       $scope.airportDist = data.airportdist; // AIRPORTDIST
-      $scope.assignData();
   });
-
-  $scope.assignData = function() {
-    $scope.airport = data.airport;
-    $scope.airportDist = data.airportdist;
-  }
-
-//end of controller
 });
